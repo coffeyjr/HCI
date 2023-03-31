e04f06deda0755bb8d6bb4da9493381bff5b6260
Jeremy <bishopjc@etsu.edu>
2023-03-30 22:01:08 -0400

    Create basic MVC app from template

---
+++ ParkingLotApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ParkingLotApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
