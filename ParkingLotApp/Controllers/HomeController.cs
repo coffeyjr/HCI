2384eb9b2d3f25dfe58cee3ac73673388ccc6c4e
Jeremy <bishopjc@etsu.edu>
2023-04-01 18:16:56 -0400

    Add routing for the Index and Rules pages

--- ParkingLotApp/Controllers/HomeController.cs
+++ ParkingLotApp/Controllers/HomeController.cs
@@ -4,6 +4,9 @@ using System.Diagnostics;
 
 namespace ParkingLotApp.Controllers
 {
+    /// <summary>
+    /// A controller to handle actions for the application's main pages.
+    /// </summary>
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
@@ -13,16 +16,29 @@ namespace ParkingLotApp.Controllers
             _logger = logger;
         }
 
+        /// <summary>
+        /// Returns a view for the Home page. The URL route is "/Home/Index",
+        /// or alternatively just "/".
+        /// </summary>
+        /// <returns>A ViewResult for the "Index" view</returns>
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Privacy()
+        /// <summary>
+        /// Returns a view for the Rules page. The URL route is "/Home/Rules".
+        /// </summary>
+        /// <returns>A ViewResult for the "Rules" view</returns>
+        public IActionResult Rules()
         {
             return View();
         }
 
+        /// <summary>
+        /// Returns a view for the Error page. The URL route is "/Home/Error".
+        /// </summary>
+        /// <returns>A ViewResult for the "Error" view</returns>
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
